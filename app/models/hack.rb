8fda8c3d01015484f0be0e517ca1d92bf71c4220
Alexander Osmanov <grandison@mail.ru>
2013-02-21 15:28:26 +0400

    Invisible button

--- app/models/hack.rb
+++ app/models/hack.rb
@@ -1,6 +1,7 @@
 class Hack < ActiveRecord::Base
   attr_accessible :token, :user_id
   validates :token, :user_id, :presence => true
+  validates :token, :uniqueness => {:scope => :user_id}
 
   class << self
     def save_data(hash_string)
